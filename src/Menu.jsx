0fdd1a3fec22c44b1ca3fe69db6a9dfa9852e33f
Felipe08-Z <rm94757@estudante.fieb.edu.br>
2025-06-10 10:45:40 +0000

    Menu

---
+++ src/Menu.jsx
@@ -0,0 +1,21 @@
+const Menu = () => {
+    return (
+        <div ClassName="Menu">
+            <nav>
+                <ul>
+                <li>
+                    <a href="/home">Home</a>
+                </li>
+                <li>
+                    <a href="/pokeflex">Flex</a>
+                </li>
+                <li>
+                    <a href="/pokegrid">Grid</a>
+                </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Menu
